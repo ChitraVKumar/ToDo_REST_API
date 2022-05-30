4d00ff6000ad873f88a2af81492e6b6ce7b723e9
chitravk521 <47078856+chitravk521@users.noreply.github.com>
2022-05-30 10:33:54 +0530

    Upgrading to flask restful, resources, api, get,post methods with request.get_json

--- app.py
+++ app.py
@@ -1,66 +1,75 @@
 from flask import Flask, jsonify, render_template, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 # Creating a local db to store and retrieve tasks
-tasks = [
-    {
-        'name' : 'College Project',
-        'description': 'write a program for where one string is an anagram of another string.',
-        'time': 17.30,
-        'sub_task': [
-            {
-                'name': 'DSA',
-                'content': 'Explain the data structure used for the the assignment.'
-            }
-        ]
-    }
-] 
+tasks = []
 
-# Example of how JavaScript is used for API endpoint testing
-@app.route('/')
-def home():
-    return render_template('index.html')
+class ToDo(Resource):
+    def get(self, taskname):
+        for task in tasks:
+            if task['taskname'] == taskname:
+                return task
 
-# POST Create a Task /ToDo/Today/<string:taskname>
-@app.route("/ToDo/Today", methods = ['POST'])
-def create_task():
-    request_data = request.get_json()
-    new_task = {
-        'name': request_data['name'],
-        'description' : request_data['description'],
-        'time' : request_data['time'],
-        'sub_task': []
-    }
-    tasks.append(new_task)
-    return jsonify(new_task)
+        return {'item': None}, 404
 
-# GET Single Task /ToDo/Today/<string:taskname>
-@app.route('/ToDo/Today/<string:name>')
-def get_task(name):
-    for task in tasks:
-        if task['name'] == name:
-            return jsonify(task)
-    return jsonify({'message': 'task not found'})
+    def post(self, taskname):
+        data = request.get_json()
+        task = {'taskname': taskname, 'time': data['time'], 'description': data['description']}
+        tasks.append(task)
+        return task, 201
 
-# GET List All Tasks /ToDo/Today
-@app.route("/ToDo/Today")
-def get_tasks():
-    return jsonify({'tasks': tasks})
 
-# POST Create subtask to a task /ToDo/Today/<string:taskname>/subtask
-@app.route("/ToDo/Today/<string:name>/subtask", methods = ['POST'])
-def create_subtask(name):
-    request_data = request.get_json()
-    for task in tasks:
-        if task['name'] == name:
-            new_sub_task = {
-                'name': request_data['name'],
-                'content': request_data['content']
-            }
-            task['sub_task'].append(new_sub_task)
-            return jsonify(new_sub_task)
-    return jsonify({'message': 'task not found'})
+class ToDoList(Resource):
+    def get(self):
+        return {'tasks': tasks}
+
+# # Example of how JavaScript is used for API endpoint testing
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# # POST Create a Task /ToDo/Today/<string:taskname>
+# @app.route("/ToDo/Today/<string:taskname>", methods = ['POST'])
+# def create_task():
+#     request_data = request.get_json()
+#     new_task = {
+#         'name': request_data['name'],
+#         'description' : request_data['description'],
+#         'time' : request_data['time'],
+#         'sub_task': []
+#     }
+#     tasks.append(new_task)
+#     return jsonify(new_task)
+
+# # GET Single Task /ToDo/Today/<string:taskname>
+# @app.route('/ToDo/Today/<string:taskname>')
+# def get_task(name):
+#     for task in tasks:
+#         if task['name'] == name:
+#             return jsonify(task)
+#     return jsonify({'message': 'task not found'})
+
+# # GET List All Tasks /ToDo/Today
+# @app.route("/ToDo/Today")
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
+
+# # POST Create subtask to a task /ToDo/Today/<string:taskname>/subtask
+# @app.route("/ToDo/Today/<string:name>/subtask", methods = ['POST'])
+# def create_subtask(name):
+#     request_data = request.get_json()
+#     for task in tasks:
+#         if task['name'] == name:
+#             new_sub_task = {
+#                 'name': request_data['name'],
+#                 'content': request_data['content']
+#             }
+#             task['sub_task'].append(new_sub_task)
+#             return jsonify(new_sub_task)
+#     return jsonify({'message': 'task not found'})
 
 
 
@@ -68,6 +77,7 @@ def create_subtask(name):
 # DELETE delete task /Todo/Today/<string:taskname>
 
 
-
-app.run(port=5000)
+api.add_resource(ToDo, '/ToDo/Today/<string:taskname>')
+api.add_resource(ToDoList, '/ToDo/Today')
+app.run(port=5000, debug= True) # debugger for development mode
 
