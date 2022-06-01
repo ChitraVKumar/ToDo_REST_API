eb64ff6c09fa3ab46b8e6c0ad205fea75778c621
chitravk521 <47078856+chitravk521@users.noreply.github.com>
2022-06-01 18:08:44 +0530

    parsing the arguments using the reqparse

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from ast import Delete
-from typing import ItemsView
+from datetime import datetime
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+import datetime
 
 
 from security import authenticate, identity
@@ -17,6 +17,13 @@ jwt = JWT(app, authenticate, identity)
 tasks = []
 
 class ToDo(Resource):
+    
+    parser = reqparse.RequestParser()
+    # parser.add_argument('time', type=lambda s: datetime.datetime.strptime(s, '%H:%M'), required= True, help='Required!')
+    parser.add_argument('time', type=float, required= True, help='Required!')
+    parser.add_argument('description', type=str, required = True, help='Required')
+
+
     @jwt_required()
     def get(self, taskname):
         task = next(filter(lambda x: x['taskname']==taskname, tasks), None)  # using the lambda instead for for loop
@@ -24,9 +31,12 @@ class ToDo(Resource):
 
     @jwt_required()
     def post(self, taskname):
+
         if next(filter(lambda x: x['taskname']==taskname, tasks), None):
             return {'message': 'A task with the name {} already exists'.format(taskname)}, 400 # if the task already exists, then return a message task already exists.
-        data = request.get_json()
+
+        data = ToDo.parser.parse_args()
+        
         task = {'taskname': taskname, 'time': data['time'], 'description': data['description']}
         tasks.append(task)
         return task, 201
@@ -39,7 +49,8 @@ class ToDo(Resource):
 
     @jwt_required()
     def put(self, taskname):
-        data = request.get_json()
+        data = ToDo.parser.parse_args()
+
         task = next(filter(lambda x: x['taskname'] == taskname, tasks), None)
         if task is None:
             task = {'taskname': taskname, 'description': data['description'], 'time': data['time']}
@@ -51,6 +62,7 @@ class ToDo(Resource):
 
 
 class ToDoList(Resource):
+    @jwt_required()
     def get(self):
         return {'tasks': tasks}
 
